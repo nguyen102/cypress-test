923c3c6f57a6fa650c6960a5c9b652234a299586
Tim Nguyen <thingut@amazon.com>
2021-08-12 17:19:32 -0400

    Add Google DNS test

--- cypress/integration/server-connection.spec.js
+++ cypress/integration/server-connection.spec.js
@@ -1,18 +1,11 @@
 describe('Example connection test', () => {
 
+    it('Google DNS server', () => {
+        const ipAddress = '8.8.8.8';
+        const port = '443';
+        cy.exec(`node checkConnection.js ${ipAddress} ${port}`).its('stdout').should('equal', "true")
+    })
     it('Invalid Ip Address connection', () => {
-        cy.visit('https://example.cypress.io/todo')
-        // We use the `cy.get()` command to get all elements that match the selector.
-        // Then, we use `should` to assert that there are two matched items,
-        // which are the two default items.
-        cy.get('.todo-list li').should('have.length', 2)
-
-        // We can go even further and check that the default todos each contain
-        // the correct text. We use the `first` and `last` functions
-        // to get just the first and last matched elements individually,
-        // and then perform an assertion with `should`.
-        cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-        cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
         const ipAddress = '1.1.1.1';
         const port = '22';
         cy.exec(`node checkConnection.js ${ipAddress} ${port}`).its('stdout').should('equal', "false")
