d7060b76abfa8c5033378454764faf543675d747
Tim Nguyen <thingut@amazon.com>
2021-08-13 13:12:16 -0400

    Return status of test command

--- run-tests-in-parallel.sh
+++ run-tests-in-parallel.sh
@@ -4,7 +4,9 @@ if [ "${TEST_ENV}" = "AppStream_Enabled" ]
 then
   echo "Run AppStream tests"
   yarn run cypress run --spec cypress/integration/AppStream_Enabled.spec.js
+  return $?
 else
   echo "Run regular workspace tests"
   yarn run cypress run --spec cypress/integration/AppStream_Disabled.spec.js
+  return $?
 fi
